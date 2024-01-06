0028dd548d8f0c46fbef00ddd9427ba05b896087
mavandrade4 <mavandrade4@gmail.com>
2024-01-06 05:40:05 +0000

    extra imgs ainda nao estao a ser bem carregadas, tenho que rever, mas os outros elementos da pagina de detalhe de um trabalho ja la estao

--- talk-bro/src/components/work_page/work_page.js
+++ talk-bro/src/components/work_page/work_page.js
@@ -3,15 +3,22 @@ import './work_page.css';
 
 function WorkPage() {
     const { state } = useLocation();
-
+    console.log(state.metadata.extra_imgs);
     //////////////////////////////////////////////// PAGINA DETALHE TRABALHO
     return (
         <div>
             <p>{state.metadata.title}</p>
             <p>{state.metadata.client}</p>
-            <p>{state.metadata.info}</p>
-            <img class="work_detail" src={state.metadata.img.url}/>
-            <p>{state.metadata.autor}</p>
+            <p>{state.metadata.description}</p>
+            <img className="work_detail" src={state.metadata.img.url}/>
+            {/*state.metadata.extra_imgs.map((extra_img) => (
+                <div>
+                    <img key={extra_img.id} src={extra_img.url} className="extra_img"/>
+                </div>
+            ))*/}
+            <p>{state.metadata.author}</p>
+            <p>{state.metadata.year}</p>
+            <p>{state.metadata.tags.join(',')}</p>
         </div>
     );
 }
