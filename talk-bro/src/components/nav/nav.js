5391c693b638c6b7e66647d774d71b407f5b6236
mavandrade4 <mavandrade4@gmail.com>
2024-01-06 20:56:03 +0000

    old apagado, alteraçoes na nav e team page nao terminadas

--- talk-bro/src/components/nav/nav.js
+++ talk-bro/src/components/nav/nav.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
-import './nav.css';
 import { Link } from 'react-router-dom';
+import './nav.css';
 
 const NavBar = () => {  
     return (
       <nav className="navbar">
         <div className="navbar-links">
+          <Link to="/">
+            <img src="img/logo.png" class="navbar-img"/>
+          </Link>
           <Link to="/">TRABALHOS</Link>
           <Link to="/team">SOBRE NÓS</Link>
           <a href="#footer">CONTACTOS</a>
           <Link to="/search">
-            <img src="img/search.png" />
+            <img src="img/search.png" class="navbar-search"/>
           </Link>
         </div>
       </nav>
