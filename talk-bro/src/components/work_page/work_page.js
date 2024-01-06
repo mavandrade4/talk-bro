d839b99c5261dd2e8e4b07a82ab40a9f6a09109c
NathaliacPosada <nathaliacposada@gmail.com>
2024-01-06 13:10:32 +0000

    Resolve conflicts in footer.css and work_page.js

--- talk-bro/src/components/work_page/work_page.js
+++ talk-bro/src/components/work_page/work_page.js
@@ -7,13 +7,6 @@ function WorkPage() {
     //////////////////////////////////////////////// PAGINA DETALHE TRABALHO
     return (
         <div>
-<<<<<<< HEAD
-            <p className="work_title">{state.metadata.title}</p>
-            <p className="work_client">{state.metadata.client}</p>
-            <p className="work_info">{state.metadata.info}</p>
-            <img className="work_detail" src={state.metadata.img.url}/>
-            <p>{state.metadata.autor}</p>
-=======
             <p>{state.metadata.title}</p>
             <p>{state.metadata.client}</p>
             <p>{state.metadata.description}</p>
@@ -26,7 +19,6 @@ function WorkPage() {
             <p>{state.metadata.author}</p>
             <p>{state.metadata.year}</p>
             <p>{state.metadata.tags.join(',')}</p>
->>>>>>> 324d46cf659afc4fc349bd1a3f31072db1e89516
         </div>
     );
 }
